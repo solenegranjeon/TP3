c4ee00a2e68bee1eb60582edb994182c2d277a07
Solene Granjeon <solene.granjeon-noriot@insa-lyon.fr>
2016-01-18 17:35:15 +0100

    Main modifiée

--- test_polymorph.cpp
+++ test_polymorph.cpp
@@ -18,45 +18,27 @@
 //                                    MAIN
 // ===========================================================================
 int main(int argc, char* argv[]) {
-  Character   character1 = Character();
   Yoshi       character2 = Yoshi();
-  Character*  character3 = new Character();
   Yoshi*      character4 = new Yoshi();
-  Character*  character7 = new Yoshi();
 
-  character1.Accelerate();
   character2.Accelerate();
-  character3->Accelerate();
   character4->Accelerate();
-  character7->Accelerate();
 
-  printf("Character 1 speed : %f\n", character1.speed());
   printf("Character 2 speed : %f\n", character2.speed());
-  printf("Character 3 speed : %f\n", character3->speed());
   printf("Character 4 speed : %f\n", character4->speed());
-  printf("Character 7 speed : %f\n", character7->speed());
 
   printf("\n" );
 
-  character1.Break();
   character2.Break();
-  character3->Break();
   character4->Break();
-  character7->Break();
 
-  printf("Character 1 speed : %f\n", character1.speed());
   printf("Character 2 speed : %f\n", character2.speed());
-  printf("Character 3 speed : %f\n", character3->speed());
   printf("Character 4 speed : %f\n", character4->speed());
-  printf("Character 7 speed : %f\n", character7->speed());
 	
 	printf("On a créé %d characters.\n",Character::nb_instances);
 	
-	character1.~Character();
 	character2.~Character();
-	delete character3;
 	delete character4;
-	delete character7;
 	
 	printf("Après les deletes, il en reste %d.\n",Character::nb_instances);
 
